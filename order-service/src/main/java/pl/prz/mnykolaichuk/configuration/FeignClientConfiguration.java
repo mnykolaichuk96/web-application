d897bbce0833ccf6d781f2ab3ac7b7006b779dd0
Mykola Nykolaichuk <52537767+mnykolaichuk96@users.noreply.github.com>
2023-06-25 11:48:27 +0200

    Add heroku.yml

--- order-service/src/main/java/pl/prz/mnykolaichuk/configuration/FeignClientConfiguration.java
+++ order-service/src/main/java/pl/prz/mnykolaichuk/configuration/FeignClientConfiguration.java
@@ -1,13 +1,14 @@
 package pl.prz.mnykolaichuk.configuration;
 
 import feign.Logger;
+import feign.RequestInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import pl.prz.mnykolaichuk.service.rest.client.InventoryFeignClient;
 
 @Configuration
 public class FeignClientConfiguration {
-
         @Bean
         Logger.Level feignLoggerLevel() {
             return Logger.Level.FULL;
